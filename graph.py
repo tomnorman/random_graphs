c0bf34021336ed8c9f9b70b1215e2ff26cffa958
tom <tom>
2020-09-10 11:42:18 +0300

    alpha and omega

--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
-import vertex
-import edge
 import numpy as np
+from itertools import chain, combinations
 from collections import deque
 
 
@@ -8,68 +7,146 @@ from collections import deque
 class mat_graph:
     '''vertices- starts from zero'''
     def __init__(self,
-            x
             ):
-        self.mat = np.array([1])[:,None]
-        self.vertices = [x]
-        self.ccs_num = 1
-        self.cc = [set([0])]
+        self.reset()
+        self.mat = np.array([])
+
+    def reset(self):
+        self.cc = [] #connected component
+        # for each cc
+        self.ps = [] #powerset
+        self.max_ind = [] #maximum independent set
+        self.max_cliq = [] #maximum clique
 
     def add_vertex(self,
-            x
+            x = 1
             ):
-        self.vertices.append(x)
-        # make matrix bigger
-        self.mat = moar(self.mat)
-        self.ccs_num = 0
+        self.reset()
+        if self.mat.size == 0:
+            self.mat = np.array([x])[:, None]
+        else:
+            # make matrix bigger
+            self.mat = moar(self.mat)
+            self.mat[-1,-1] = x
     
     def add_edge(self,
             ver1,
             ver2,
-            w
+            w = 1
             ):
+        self.reset()
         self.mat[ver1, ver2] = w
         self.mat[ver2, ver1] = w
-        self.ccs_num = 0
 
+    # connected components
     def ccs(self):
-        if not self.ccs_num:
-            vertices_num = self.mat.shape[0]
-            visited = [0 for _ in range(vertices_num)]
-            self.ccs_num = 0
-            self.cc = []
-            for v in range(vertices_num):
-                if not visited[v]:
-                    self.ccs_num += 1
-                    self.cc += [set(bfs(self.mat, v, [v], visited))]
-        return({"ccs_num":self.ccs_num, "ccs":self.cc})
+        if self.cc: return
+
+        vertices_num = self.mat.shape[0]
+        visited = np.full(vertices_num, False)
+        self.ccs_num = 0
+        self.cc = []
+        for v in range(vertices_num):
+            if not visited[v]:
+                self.cc += [(bfs(self.mat, v, [v], visited))]
+
+    # powersets of each cc
+    def pss(self):
+        self.ccs()
+        if self.ps: return
+
+        for cc in self.cc:
+            self.ps += [list(powerset(cc))]
+
+
+    def maxes(self,
+            l, #list
+            t
+            ):
+        self.pss()
+        if l: return
+
+        for ps in self.ps:
+            l += [maxes(self.mat, ps, t)]
+
+
+    def alpha(self,
+            debug = False
+            ):
+        self.maxes(self.max_ind, 'i')
+        if debug:   print(self.max_ind)
+        max_ind_size = sum([len(ind) for ind in self.max_ind])
+        return(max_ind_size)
+
+
+    def omega(self,
+            debug = False
+            ):
+        self.maxes(self.max_cliq, 'c')
+        if debug:   print(self.max_cliq)
+        max_cliq_size = max([len(cliq) for cliq in self.max_cliq])
+        return(max_cliq_size)
+
 
     def __str__(self):
         print(self.mat)
-        return(str(self.vertices))
+        return('')
     def __repr__(self):
         return(self.__str__())
 
 
 ######################### functions
-def bfs(mat, v, d, visited):
-    ns = neighbors(mat, v, visited)
-    d += ns
-    for j in ns:
-        bfs(mat, j, d, visited)
+def bfs(mat,
+        v, #current vertex- scalar
+        d, #current connected component- list
+        visited
+        ):
+    for j in neighbors(mat, v, visited):
+        if not visited[j]:
+            d += [j]
+            bfs(mat, j, d, visited)
     return(d)
 
 
-def neighbors(mat, v, visited):
+def neighbors(
+        mat,
+        v, #current vertex- scalar
+        visited
+        ):
     ns = []
     if not visited[v]:
-        visited[v] = 1
-        for n, e in enumerate(mat[v,:]):
-            if visited[n]:
+        visited[v] = True
+        for j, e in enumerate(mat[v,:]):
+            if visited[j]:
                 continue
-            if e > 0:
-                ns.append(n)
+            if e != 0:
+                ns.append(j)
     return(ns)
+
+def powerset(
+        iterable
+        ):
+    "powerset([1,2,3]) --> (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(1,len(s)+1))
+
+def maxes(
+        mat,
+        ps, #powerset- list of tupels
+        t #type of max- clique or independent
+        ):
+    ind = []
+    for s in ps:
+        sub_mat = mat[s,:][:,s]
+        sub_mat[np.diag_indices_from(sub_mat)] = 1 if t == 'c' else 0
+        condition = np.all(sub_mat) if t == 'c' else not np.any(sub_mat)
+
+        if condition:
+            if len(s) > len(ind):
+                ind = s
+    return(ind)
+                
+
 
 
 def moar(
@@ -79,20 +156,31 @@ def moar(
     row = np.zeros(mat.shape[0] + 1)[None, :]
     mat = np.hstack((mat, col))
     mat = np.vstack((mat, row))
-    mat[-1, -1] = 1
     return(mat)
 
-a=mat_graph(1)
+a=mat_graph()
+a.add_vertex(10)
+a.add_vertex(10)
 a.add_vertex(1)
 a.add_vertex(1)
-a.add_vertex(1)
-a.add_vertex(1)
-a.add_edge(0,1,1)
-a.add_edge(0,2,1)
-a.add_edge(1,2,1)
-#a.add_edge(1,4,1)
-a.add_edge(3,4,1)
+a.add_vertex(3)
+a.add_vertex(3)
+a.add_vertex(3)
+a.add_vertex(3)
+a.add_vertex(3)
+a.add_edge(2,3)
+a.add_edge(2,8)
+a.add_edge(8,7)
+# cc
+a.add_edge(0,1)
+a.add_edge(0,8)
+a.add_edge(0,6)
+a.add_edge(0,5)
+a.add_edge(1,8)
+a.add_edge(1,5)
+a.add_edge(1,6)
+a.add_edge(6,8)
+a.add_edge(6,5)
+a.add_edge(8,5)
 
-b=mat_graph(1)
-b.add_vertex(1)
-#import runpy; a=runpy.run_module("graph"); a['a']; a['bfs'](a['a'].mat,0,[0],[0 for _ in range(5)])
+#import runpy; a=runpy.run_module("graph"); a['a']; a['a'].omega(True)
